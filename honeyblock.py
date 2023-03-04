9f2bda63c6efaf854ddc34a2c10e55c717c0fd13
Stuart Gray <42702437+m00tiny@users.noreply.github.com>
2023-03-04 01:31:47 -0500

    Added a "honey script" to detect people who don't know which services are available on the host and subsequently ban them.

---
+++ honeyblock.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+ip = "10.0.0.10"
+ports = [53, 80]
+honeys = [8000, 8443]
+
+blocked = []
+
+def analyzePackets(p):
+    global blocked
+    if p.haslayer(IP):
+        response = Ether(src=p[Ether].dst,dst=p[Ether].src/\
+            IP(src=p[IP].dst,dst=p[IP].src)/\
+            TCP(sport=p[TCP].dport,dport[TCP].sport,ack=p[TCP].seq+1)
+        source = p[IP].src
+    else:
+        response = Ether(src=p[Ether].dst,dst=p[Ether].src)/\
+            IPv6(src=p[IPv6].dst,dst=p[IPv6].src)/\
+            TCP(sport=p[TCP].dport,dport[TCP].sport,ack=p[TCP].seq+1)
+        source = p[IPv6].src
+    if p[TCP].flags != "S":
+        return
+    port = p[TCP].dport
+    if source in blocked:
+        if port in ports:
+            response[TCP].flags = "RA"
+            print("Sending reset")
+        elif port in honeys:
+            response[TCP].flags = "SA"
+        else:
+            return
+        sendp(response,verbose=False)
+    else:
+        if port not in ports:
+            blocked += [source]
+            if port in honeys:
+                response[TCP].flags = "SA"
+                sendp(response,verbose=False)
+
+f = "dst host "+ip+" and tcp"
+sniff(filter=f,prn=analyzePackets)
